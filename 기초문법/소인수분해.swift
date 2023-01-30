816b883c25adc402cd2aba3ef1670206470e2375
singyuKang <rkdtlscks123@naver.com>
2023-01-30 17:55:07 +0900

    Update Swift_Cording_Test

---
+++ 기초문법/소인수분해.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    
+    var number = n
+    var answer:[Int] = []
+    
+    for i in 2...n{
+        // print(i)
+        while(number % i == 0){
+            answer.append(i)
+            number = number / i
+        }
+    }
+    
+    
+    
+    return Array(Set(answer)).sorted()
+
+}
