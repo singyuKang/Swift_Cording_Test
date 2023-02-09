51c6c2e932345f5ea0919c5465fc1cec6817fea0
singyuKang <rkdtlscks123@naver.com>
2023-02-09 18:25:15 +0900

    Update Swift_Cording_Test

--- 기초문법/탐욕법(Greedy)/체육복.swift
+++ 기초문법/탐욕법(Greedy)/체육복.swift
@@ -46,19 +46,19 @@ func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
 //참고
 func solution1(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
 
-    // 가지고 있는 체육복 개수 저장 (기본적으로 1벌)
+
     var students = [Int](repeating: 1, count: n)
     
-    // 잃어버린 사람은 체육복 0벌
+
     for l in lost {
         students[l-1] = 0
     }
-    // 여벌 가져 온 사람은 +1 벌
+
     for r in reserve {
         students[r-1] += 1
     }
     
-    var count = 0 // 체육복을 빌리지 못한 학생 수
+    var count = 0
     for i in 0..<n {
         if students[i] == 0 {
             if i>0 && students[i-1] > 1 {
