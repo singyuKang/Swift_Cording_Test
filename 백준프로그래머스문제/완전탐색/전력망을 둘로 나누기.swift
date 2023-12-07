b40ce10dc4c381e025690f99ad15e07158abc228
singyuKang <rkdtlscks123@naver.com>
2023-12-07 18:22:08 +0900

    Update Swift_Cording_Test

---
+++ 백준프로그래머스문제/완전탐색/전력망을 둘로 나누기.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+//BFS 이용
+func solution(_ n:Int, _ wires:[[Int]]) -> Int {
+    
+    var answer = 9999
+
+    for i in 0..<wires.count {
+        var isVisitedNode : [Int] = Array(repeating : 0 , count : n+1)
+        var wireGraph : [[Int]] = Array(repeating  : Array(repeating : 0, count : n+1) , count : n+1)
+        var queue : [Int] = []
+        var count = 0
+        // init
+        for j in 0..<wires.count {
+            if j == i {
+                continue
+            }
+            wireGraph[wires[j][0]][wires[j][1]] = 1
+            wireGraph[wires[j][1]][wires[j][0]] = 1
+        }
+        
+       
+        queue.append(1)
+        isVisitedNode[1] = 1
+        
+        while !queue.isEmpty {
+            var first = queue.removeFirst()
+            for (index , k) in wireGraph[first].enumerated(){
+                if isVisitedNode[index] == 0 && wireGraph[index][first] == 1 {
+                    queue.append(index)
+                    isVisitedNode[index] = 1
+                }
+            }
+        }
+    
+        for j in isVisitedNode {
+            if j == 1 {
+                count += 1
+            }
+        }
+
+        let tempAnswer = abs((n-count) - count)
+        
+        if tempAnswer < answer {
+            answer = tempAnswer
+        }
+
+    }
+    
+     
+    return answer
+}
