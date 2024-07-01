3b12a6a4e54d06eb61414e6dc4d10756d97c9623
singyuKang <rkdtlscks123@naver.com>
2024-07-01 17:37:56 +0900

    [feat] : 이진탐색 입국심사 백준

--- 백준프로그래머스문제/이진탐색/입국심사.swift
+++ 백준프로그래머스문제/이진탐색/입국심사.swift
@@ -33,3 +33,37 @@ func solution(_ n:Int, _ times:[Int]) -> Int64 {
     
     return Int64(result)
 }
+
+//백준 3079번 https://www.acmicpc.net/problem/3079
+var input = readLine()!.split(separator: " ").map { Int(String($0))! }
+var (N,M) = (input[0] , input[1])
+var timeArr : [Int] = []
+for _ in 0..<N {
+    let value = Int(readLine()!)!
+    timeArr.append(value)
+}
+timeArr.sort {
+    $0 < $1
+}
+
+var answer = 0
+var start = 1
+var end = timeArr[timeArr.count-1] * M
+
+while start <= end {
+    let mid = (start + end) / 2
+    var people = 0
+    for i in 0..<timeArr.count {
+        people += mid / timeArr[i]
+    }
+
+    if M > people {
+        start = mid + 1
+    }else{
+        answer = mid
+        end = mid - 1
+    }
+
+}
+
+print(answer)
